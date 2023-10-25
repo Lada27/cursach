bf524b23cc71f063368cd5854116135ac3c1154d
Lada27 <ymka26@bk.ru>
2023-10-25 16:29:31 +0300

    commit Lada test

--- app/src/main/java/com/example/cursach/MainActivity.kt
+++ app/src/main/java/com/example/cursach/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.cursach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val greetingTextView = findViewById<TextView>(R.id.tvHello)
+        val inputField = findViewById<EditText>(R.id.tvName)
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+        submitButton.setOnClickListener{
+            val entredName = inputField.text.toString()
+            val message = "Hello $entredName"
+            greetingTextView.text = message
+
+        }
+
     }
 }
